4886086e7882e6f10bd9736bd3c1db470f62236f
stfn <steffenriis@outlook.com>
2024-01-04 02:26:02 +0100

    feat: added widget to dashboard

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\File;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make(label:"Files Stored", value:File::Count())
+            ->description("Total amount of files Stored in Storage")
+        ];
+    }
+}
